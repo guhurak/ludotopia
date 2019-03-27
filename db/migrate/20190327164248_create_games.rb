792aa0e3899c5e95247465e6f61cf728669640ad
guhurak <florianeven.pro@laposte.net>
2019-03-27 17:45:21 +0100

    add columns to game table

--- db/migrate/20190327164248_create_games.rb
+++ db/migrate/20190327164248_create_games.rb
@@ -2,6 +2,10 @@ class CreateGames < ActiveRecord::Migration[5.2]
   def change
     create_table :games do |t|
 
+    	t.string :title,			null: false
+    	t.text :description
+    	t.string :link,				null: false, default: ""
+
       t.timestamps
     end
   end
